e5d6e4761dd23066b820df41a2963edfe28fe880
Noritoshi Atsumi <atsumi@nagoya-u.jp>
2020-07-11 14:11:44 +0900

    add starting message

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from slacker import Slacker
 from slackbot.bot import Bot
 from tododb import DB
 import os
+
+slack = Slacker(os.environ['API_TOKEN'])
+channel = os.environ['SLACK_CHANNEL']
+slack.chat.post_message(channel, 'new version bot is deployed.', as_user=True)
 
 dbname = os.environ['TODO_DB']
 db = DB(dbname)
