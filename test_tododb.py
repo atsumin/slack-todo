64e839857c519ff00ce42be4da943d635cdab4c9
ryok21 <ryo.k1@outlook.com>
2020-07-13 21:59:22 +0900

    修正

--- test_tododb.py
+++ test_tododb.py
@@ -4,7 +4,7 @@ import os
 dbname = os.environ['TODO_DB']
 database = DB(dbname)
 if not os.path.exists(dbname):
-    databae.init()
+    database.init()
 
 database.add('test-title1', '2020-06-01')
 database.list()
