ed9bb580536cdd121e90a795b755415f79cd5c90
森正好美 <morimasa.yoshimi.46r@st.kyoto-u.ac.jp>
2020-07-26 15:58:42 +0900

    first commit

--- tododb.py
+++ tododb.py
@@ -2,14 +2,14 @@ import sqlite3
 import datetime
 
 class DB(object):
-    def __init__(self, name):
+    def __init__(self,name):
         self.__conn = sqlite3.connect(name)
         self.__c = self.__conn.cursor()
 
     def __create_table(self):
         self.__c.execute("create table todo" \
                          "(id integer NOT NULL PRIMARY KEY AUTOINCREMENT, " \
-                          "title text NOT NULL, limit_at text, update_at text NOT NULL)")
+                          "title text NOT NULL, limit_at text, update_at text NOT NULL, careful text NOT NULL)")
 
     def __drop_table(self):
         self.__c.execute("drop table todo")
@@ -21,10 +21,10 @@ class DB(object):
         self.__drop_table()
         self.__create_table()
 
-    def add(self, title, limit_at):
+    def add(self, title, limit_at, careful):
         update_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        sql = "insert into todo (title, limit_at, update_at) values (?, ?, ?)"
-        self.__c.execute(sql, [title, limit_at, update_at])
+        sql = "insert into todo (title, limit_at, update_at, careful) values (?, ?, ?, ?)"
+        self.__c.execute(sql, [title, limit_at, update_at, careful])
         self.__conn.commit()
 
     def list(self):
