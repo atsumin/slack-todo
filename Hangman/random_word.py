aed9bd46fcc664ea1daa99d7033c2cb6f6e5ed46
haruki <omatsu.haruki.43c@st.kyoto-u.ac.jp>
2020-08-03 20:34:01 +0900

    Hangman ,Numer0n（未完成）フォルダの追加

---
+++ Hangman/random_word.py
@@ -0,0 +1,3 @@
+words=list=["ax","boy","github","fake","champion","queen","elephant","typhoon","children","cherry","fireworks","doctor","chemistry",
+            "mouse","genius","chocolate","mathematics","newton","halloween","physics","rhythm"]
+            
