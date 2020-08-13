538b391d2e33e32e17f6957ed8fdbc37b6f672f5
haruki <omatsu.haruki.43c@st.kyoto-u.ac.jp>
2020-08-14 02:24:38 +0900

    Hangman #1

--- plugins/Hangman/check_word.py
+++ plugins/Hangman/check_word.py
@@ -1,1 +1,20 @@
-check the word 
+def check_word(answer):
+    hoge="*"*(len(answer))
+    print(hoge)
+    counter=0
+    while hoge != answer:
+        guess=input("guess alphabet or answer! ")
+        if len(guess)==1:
+            if guess in answer:
+                for i in range(len(answer)):
+                    if answer[i]==guess:
+                        hoge=hoge[0:i]+guess+hoge[i+1:len(hoge)]
+        else:
+            if guess==answer:
+                hoge=answer                
+        print(hoge)
+        counter=counter+1
+    print("Right! You took "+str(counter)+" times")
+
+fuga=input("Let's Hangman! ")
+check_word(fuga)
